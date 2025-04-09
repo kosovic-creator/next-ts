975828f6b775a0f3bebfcb3d29efb0fdb4f0887b
kosovic-creator <drasko.kosovic@icloud.com>
2025-04-09 08:30:20 +0200

    init

--- app/state_reducer/page.tsx
+++ app/state_reducer/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 import { Button } from '@/components/ui/button'
-import { useReducer, useState } from 'react'
+import { useReducer, useState} from 'react'
 
 interface State {
-  count: any
+  count:number
 }
 
 interface Action {
@@ -11,31 +11,32 @@ interface Action {
   payload?: number;
 }
 
-function reducer(state: any, action: any): State {
-  console.log('to je :', state, action);
-  return state + action; // Placeholder return to avoid errors
+function reducer(state: State, action: any): State {
+  console.log('to je :',state, action);
+  return {count: state.count - (action.payload ||1)} // Placeholder return to avoid errors
 }
 
 
 export default function StateReducer() {
-  // const[count, setCount] = useState(0)
-  const [count, dispatch] = useReducer(reducer, { count: 0 })
-  const increment = () => {
-    // setCount(count + 1)
-    dispatch(1)
-    console.log('increment', count);
-  }
-  const decrement = () => {
-    // setCount(count - 1)
-    dispatch(-1)
-    console.log('decrement', count);
-  }
+    // const[count, setCount] = useState(0)
+    const[count,dispatch] = useReducer(reducer, { count: 0 })
+    const increment = () => {
+        // setCount(count + 1)
+        dispatch( 1)
+        // setCount(count + 1)
+        console.log('increment', count);
+    }
+    const decrement = () => {
+        // setCount(count - 1)
+        dispatch(-1)
+        console.log('increment', count);
+    }
   return (
     <div>
       <h1>Counter</h1>
-      <Button onClick={() => increment()}>+</Button>
+      <Button onClick={()=>increment()}>+</Button>
       <p>Count: {count.count}</p>
-      <Button onClick={() => decrement()}>-</Button>
+      <Button onClick={()=>decrement()}>-</Button>
 
     </div>
   )
