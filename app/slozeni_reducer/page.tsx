5005cd1c63cdcf970dacd1db7081cfdf9e17b0da
kosovic-creator <drasko.kosovic@icloud.com>
2025-04-10 06:53:42 +0200

    uplata

--- app/slozeni_reducer/page.tsx
+++ app/slozeni_reducer/page.tsx
@@ -5,53 +5,48 @@ import React, { useReducer } from 'react';
 
 export default function AppReducerSlozeni() {
   interface State {
-    agent: string;
-    turtleneckType: string;
-    turtleneckBlackness: number;
+    banka: string;
+    iznos: number;
   }
 
   interface Action {
-    type: 'DARKEN' | 'LIGHTEN';
+    type: 'UPLATA' | 'ISPLATA';
   }
 
   const reducer = (state: State, action: Action): State => {
     switch (action.type) {
-      case 'DARKEN':
-        if (state.agent === 'Archer' && state.turtleneckType === 'tactical') {
+      case 'UPLATA':
+        if (state.banka === 'NLB' && state.iznos <150) {
           return {
             ...state,
-            turtleneckBlackness: state.turtleneckBlackness + 10,
+            iznos: state.iznos + 10,
           };
         }
-        if (state.agent !== 'Archer') {
-          return state;
-        }
-        return state;
-      case 'LIGHTEN':
-        return {
-            ...state,
-            turtleneckBlackness: state.turtleneckBlackness -10
-          };
+        case 'ISPLATA':
+          if (state.banka === 'NLB' && state.iznos <150) {
+            return {
+              ...state,
+              iznos: state.iznos - 10,
+            };
+          }
       default:
         return state;
     }
   };
 
   const initialState = {
-    agent: 'Archer',
-    turtleneckType: 'tactical',
-    turtleneckBlackness: 100,
+    banka: 'NLB',
+    iznos: 100,
   };
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div>
-      <h1>Agent: {state.agent}</h1>
-      <p>Turtleneck Type: {state.turtleneckType}</p>
-      <p>Turtleneck Blackness: {state.turtleneckBlackness}</p>
-      <Button onClick={() => dispatch({ type: 'DARKEN' })}>Darken</Button>
-      <Button onClick={() => dispatch({ type: 'LIGHTEN' })}>Lighten</Button>
+      <h1>Banka: {state.banka}</h1>
+      <p>Sada je iznos: {state.iznos}</p>
+      <Button className='bg-amber-950 size-fit' onClick={() => dispatch({ type: 'UPLATA' })}>Uplata</Button>
+      <Button onClick={() => dispatch({ type: 'ISPLATA' })}>Ispalta</Button>
     </div>
   );
 }
