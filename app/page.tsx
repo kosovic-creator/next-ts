abae74ca25450c1edc332377fbf93c7f595203fb
kosovic-creator <drasko.kosovic@icloud.com>
2025-04-11 06:37:57 +0200

    m

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,14 @@ import Mail from './mail/page';
 
 
 export default function Home() {
-    const [username, setUsername] = useState("");
-    const [showProfile, setShowProfile] = useState(false);
+  const [username, setUsername] = useState("");
+  const [showProfile, setShowProfile] = useState(false);
   return (
     <>
       <div className="App">
         <LoginContext.Provider value={{ username, setUsername, showProfile, setShowProfile }}>
           {showProfile ? <Profile /> : <Login />}
           {showProfile ? <Mail /> : null}
-
         </LoginContext.Provider>
       </div>
     </>
