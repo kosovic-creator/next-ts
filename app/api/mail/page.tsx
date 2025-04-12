17882d8073f18467a2d0e1e6281f9b0d81ea0f13
kosovic-creator <drasko.kosovic@icloud.com>
2025-04-12 07:51:49 +0200

    mail2

--- app/api/mail/page.tsx
+++ app/api/mail/page.tsx
@@ -1,18 +1,22 @@
 import { Resend } from 'resend';
-
+import options from "@/app/api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+import { Session } from "next-auth";
 const resend = new Resend('re_LPS8fWMD_E1bpqA5x8wqJLY1gXUoEEyWP');
 
 const MailPage = async () => {
+  const session = await getServerSession(options);
   await resend.emails.send({
     from: 'onboarding@resend.dev',
-    to: 'drasko.kosovic@icloud.com',
-    subject: 'Drasko, welcome to Resend!',
-    html: '<p>Drasko</p>',
+    to: session?.user?.email || 'no-reply@example.com',
+    subject: session?.user?.name || 'Welcome to Resend',
+    html: `<p>${session?.user?.name}</p>`,
   });
 
   return (
     <div>
       <h1>Email Sent</h1>
+      {/* {session?.user?.email ? <p>{session.user.email}</p> : <p>No email available</p>} */}
       <p>Check your inbox for the welcome email.</p>
     </div>
   );
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,19 +1,36 @@
 import options from "@/app/api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
+import { Session } from "next-auth";
+
+// Extend the Session type to include the role property
+declare module "next-auth" {
+  interface Session {
+    user: {
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+      role?: string | null; // Add role property
+    };
+  }
+}
 import Link from "next/link";
 import {  HomeIcon } from "lucide-react";
+import MailPage from "@/app/api/mail/page";
+// import MailPage from "@/app/api/mail/page";
 
 
 const Nav = async () => {
   const session = await getServerSession(options);
   //console.log(session);
   return (
+    <>
+ {/* <MailPage/> */}
     <header className="bg-black text-white">
       <nav className="flex justify-between items-center w-full px-10 py-4">
       <div><HomeIcon/></div>
         <div className="flex gap-10">
           <Link href="/">Home</Link>
-          {session?.user.role == "ADMIN" && (
+          {session?.user?.role == "ADMIN" && (
             <Link href="/CreateUser">Create User</Link>
           )}
           <Link href="/ClientMember">Client Member</Link>
@@ -23,6 +40,7 @@ const Nav = async () => {
             <>
               <p>{session.user.name}</p>
               <p>{session.user.email}</p>
+              <MailPage/>
               <Link href="/api/auth/signout?callbackUrl=/">Logout</Link>
             </>
           ) : (
@@ -31,6 +49,8 @@ const Nav = async () => {
         </div>
       </nav>
     </header>
+    </>
+
   );
 };
 export default Nav;
